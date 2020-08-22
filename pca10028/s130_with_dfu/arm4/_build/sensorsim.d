63052554c89b0cc088febfff90018edfab27bfd1
kejeon <kejeon@connect.ust.hk>
2020-08-22 23:05:51 +0800

    Remover old

--- pca10028/s130_with_dfu/arm4/_build/sensorsim.d
+++
@@ -1,4 +0,0 @@
-.\_build\sensorsim.o: ..\..\..\..\..\..\components\libraries\sensorsim\sensorsim.c
-.\_build\sensorsim.o: ..\..\..\..\..\..\components\libraries\sensorsim\sensorsim.h
-.\_build\sensorsim.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\_build\sensorsim.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
